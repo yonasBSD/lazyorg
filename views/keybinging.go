aad676bda9bbe5bf7e29735fcf1efad9a3858343
HubertBel <hubertolino@icloud.com>
2024-10-28 21:53:26 -0400

    Update keybindings

--- views/keybinging.go
+++ views/keybinging.go
@@ -1,72 +1,38 @@
 package views
 
 import (
-	"github.com/jroimartin/gocui"
+    "github.com/jroimartin/gocui"
 )
 
 func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
+    return gocui.ErrQuit
 }
 
 func InitKeybindings(g *gocui.Gui, av *AppView) error {
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		return err
-	}
+    if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+        return err
+    }
 
-	if err := g.SetKeybinding("", 'a', gocui.ModNone,
-		func(g *gocui.Gui, v *gocui.View) error {
-            return av.ShowPopup(g)
-		}); err != nil {
-		return err
-	}
+    mainKeybindings := []struct {
+        key     interface{}
+        handler func(*gocui.Gui, *gocui.View) error
+    }{
+        {'a', func(g *gocui.Gui, v *gocui.View) error { return av.ShowPopup(g) }},
+        {'h', func(g *gocui.Gui, v *gocui.View) error { av.UpdateToPrevDay(); return nil }},
+        {'l', func(g *gocui.Gui, v *gocui.View) error { av.UpdateToNextDay(); return nil }},
+        {'j', func(g *gocui.Gui, v *gocui.View) error { av.UpdateToNextTime(); return nil }},
+        {'k', func(g *gocui.Gui, v *gocui.View) error { av.UpdateToPrevTime(); return nil }},
+        {'H', func(g *gocui.Gui, v *gocui.View) error { av.UpdateToPrevWeek(); return nil }},
+        {'L', func(g *gocui.Gui, v *gocui.View) error { av.UpdateToNextWeek(); return nil }},
+    }
 
-	if err := g.SetKeybinding("", 'h', gocui.ModNone,
-		func(g *gocui.Gui, v *gocui.View) error {
-			av.UpdateToPrevDay()
-			return nil
-		}); err != nil {
-		return err
-	}
+    for _, viewName := range weekdayNames {
+        for _, kb := range mainKeybindings {
+            if err := g.SetKeybinding(viewName, kb.key, gocui.ModNone, kb.handler); err != nil {
+                return err
+            }
+        }
+    }
 
-	if err := g.SetKeybinding("", 'l', gocui.ModNone,
-		func(g *gocui.Gui, v *gocui.View) error {
-			av.UpdateToNextDay()
-			return nil
-		}); err != nil {
-		return err
-	}
-
-	if err := g.SetKeybinding("", 'j', gocui.ModNone,
-		func(g *gocui.Gui, v *gocui.View) error {
-			av.UpdateToNextTime()
-			return nil
-		}); err != nil {
-		return err
-	}
-
-	if err := g.SetKeybinding("", 'k', gocui.ModNone,
-		func(g *gocui.Gui, v *gocui.View) error {
-			av.UpdateToPrevTime()
-			return nil
-		}); err != nil {
-		return err
-	}
-
-	if err := g.SetKeybinding("", 'H', gocui.ModNone,
-		func(g *gocui.Gui, v *gocui.View) error {
-			av.UpdateToPrevWeek()
-			return nil
-		}); err != nil {
-		return err
-	}
-
-	if err := g.SetKeybinding("", 'L', gocui.ModNone,
-		func(g *gocui.Gui, v *gocui.View) error {
-			av.UpdateToNextWeek()
-			return nil
-		}); err != nil {
-		return err
-	}
-
-	return nil
+    return nil
 }
