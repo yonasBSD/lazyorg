56390311b440de4001f9c996fff6b5d1ef084585
HubertBel <hubertolino@icloud.com>
2024-12-09 21:43:50 -0500

    Add validator on text input

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,6 +2,9 @@ package utils
 
 import (
 	"fmt"
+	"math"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -23,7 +26,6 @@ func FormatHour(hour, minute int) string {
 }
 
 func TimeToPosition(t time.Time, s string) int {
-
     time := FormatHourFromTime(t)
 	lines := strings.Split(s, "\n")
 
@@ -34,4 +36,82 @@ func TimeToPosition(t time.Time, s string) int {
 	}
 
 	return -1
+}
+
+func ValidateTime(value string) bool {
+	regex := regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$`)
+	
+	if !regex.MatchString(value) {
+		return false
+	}
+	
+	parts := strings.Split(value, " ")
+	dateParts := strings.Split(parts[0], "-")
+	timeParts := strings.Split(parts[1], ":")
+	
+	year, err := strconv.Atoi(dateParts[0])
+	if err != nil || year <= 0 {
+		return false
+	}
+	
+	month, err := strconv.Atoi(dateParts[1])
+	if err != nil || month <= 0 || month > 12 {
+		return false
+	}
+	
+	day, err := strconv.Atoi(dateParts[2])
+	if err != nil || day <= 1 || day > 31 {
+		return false
+	}
+	
+	hours, err := strconv.Atoi(timeParts[0])
+	if err != nil || hours < 0 || hours > 23 {
+		return false
+	}
+	
+	minutes, err := strconv.Atoi(timeParts[1])
+	if err != nil || (minutes != 0 && minutes != 30) {
+		return false
+	}
+	
+	_, err = time.Parse("2006-01-02 15:04", value)
+	return err == nil
+}
+
+func ValidateName(value string) bool {
+    if value == "" {
+        return false
+    }
+
+    return true
+}
+
+func ValidateNumber(value string) bool {
+    n, err := strconv.Atoi(value)
+    if err != nil {
+        return false
+    }
+
+    if n <= 0 {
+        return false
+    }
+
+    return true
+}
+
+func ValidateDuration(value string) bool {
+    duration, err := strconv.ParseFloat(value, 64)
+    if err != nil {
+        return false
+    }
+
+    if duration <= 0.0 {
+        return false
+    }
+
+    if math.Mod(duration, 0.5) != 0 {
+         return false
+    }
+
+    return true
 }
--- pkg/views/popup-view.go
+++ pkg/views/popup-view.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/HubertBel/lazyorg/internal/calendar"
 	"github.com/HubertBel/lazyorg/internal/database"
+	"github.com/HubertBel/lazyorg/internal/utils"
 	component "github.com/j-04/gocui-component"
 	"github.com/jroimartin/gocui"
 )
@@ -39,12 +40,12 @@ func (epv *EventPopupView) Update(g *gocui.Gui) error {
 func (epv *EventPopupView) NewEventForm(g *gocui.Gui, title, name, time, location, duration, frequency, occurence, description string) *component.Form {
 	form := component.NewForm(g, title, epv.X, epv.Y, epv.W, epv.H)
 
-	form.AddInputField("Name", LabelWidth, FieldWidth).SetText(name)
-	form.AddInputField("Time", LabelWidth, FieldWidth).SetText(time)
+    form.AddInputField("Name", LabelWidth, FieldWidth).SetText(name).AddValidate("Invalid name", utils.ValidateName)
+	form.AddInputField("Time", LabelWidth, FieldWidth).SetText(time).AddValidate("Invalid time", utils.ValidateTime)
 	form.AddInputField("Location", LabelWidth, FieldWidth).SetText(location)
-	form.AddInputField("Duration", LabelWidth, FieldWidth).SetText(duration)
-	form.AddInputField("Frequency", LabelWidth, FieldWidth).SetText(frequency)
-	form.AddInputField("Occurence", LabelWidth, FieldWidth).SetText(occurence)
+	form.AddInputField("Duration", LabelWidth, FieldWidth).SetText(duration).AddValidate("Invalid duration", utils.ValidateDuration)
+	form.AddInputField("Frequency", LabelWidth, FieldWidth).SetText(frequency).AddValidate("Invalid frequency", utils.ValidateNumber)
+	form.AddInputField("Occurence", LabelWidth, FieldWidth).SetText(occurence).AddValidate("Invalid occurence", utils.ValidateNumber)
 	form.AddInputField("Description", LabelWidth, FieldWidth).SetText(description)
 
 	return form
@@ -53,10 +54,10 @@ func (epv *EventPopupView) NewEventForm(g *gocui.Gui, title, name, time, locatio
 func (epv *EventPopupView) EditEventForm(g *gocui.Gui, title, name, time, location, duration, description string) *component.Form {
 	form := component.NewForm(g, title, epv.X, epv.Y, epv.W, epv.H)
 
-	form.AddInputField("Name", LabelWidth, FieldWidth).SetText(name)
-	form.AddInputField("Time", LabelWidth, FieldWidth).SetText(time)
+	form.AddInputField("Name", LabelWidth, FieldWidth).SetText(name).AddValidate("Invalid name", utils.ValidateName)
+	form.AddInputField("Time", LabelWidth, FieldWidth).SetText(time).AddValidate("Invalid time", utils.ValidateTime)
 	form.AddInputField("Location", LabelWidth, FieldWidth).SetText(location)
-	form.AddInputField("Duration", LabelWidth, FieldWidth).SetText(duration)
+	form.AddInputField("Duration", LabelWidth, FieldWidth).SetText(duration).AddValidate("Invalid duration", utils.ValidateNumber)
 	form.AddInputField("Description", LabelWidth, FieldWidth).SetText(description)
 
 	return form
@@ -68,15 +69,12 @@ func (epv *EventPopupView) ShowNewEventPopup(g *gocui.Gui) error {
 	}
 
 	epv.Form = epv.NewEventForm(g, "New Event", "", epv.Calendar.CurrentDay.Date.Format(TimeFormat), "", "1.0", "7", "1", "")
+
+	epv.addKeybind(gocui.KeyEsc, epv.Close)
+    epv.addKeybind(gocui.KeyEnter, epv.AddEvent)
+
 	epv.Form.AddButton("Add", epv.AddEvent)
 	epv.Form.AddButton("Cancel", epv.Close)
-
-	if err := epv.setPopupKeybind(g, gocui.KeyEsc, gocui.ModNone, epv.Close); err != nil {
-		return err
-	}
-	if err := epv.setPopupKeybind(g, gocui.KeyEnter, gocui.ModNone, epv.AddEvent); err != nil {
-		return err
-	}
 
 	epv.Form.SetCurrentItem(0)
 	epv.IsVisible = true
@@ -104,16 +102,11 @@ func (epv *EventPopupView) ShowEditEventPopup(g *gocui.Gui, eventView *EventView
 	editHandler := func(g *gocui.Gui, v *gocui.View) error {
         return epv.EditEvent(g, v, event)
 	}
+	epv.addKeybind(gocui.KeyEsc, epv.Close)
+    epv.addKeybind(gocui.KeyEnter, editHandler)
 
 	epv.Form.AddButton("Edit", editHandler)
 	epv.Form.AddButton("Cancel", epv.Close)
-
-	if err := epv.setPopupKeybind(g, gocui.KeyEsc, gocui.ModNone, epv.Close); err != nil {
-		return err
-	}
-	if err := epv.setPopupKeybind(g, gocui.KeyEnter, gocui.ModNone, editHandler); err != nil {
-		return err
-	}
 
 	epv.Form.SetCurrentItem(0)
 	epv.IsVisible = true
@@ -123,6 +116,12 @@ func (epv *EventPopupView) ShowEditEventPopup(g *gocui.Gui, eventView *EventView
 }
 
 func (epv *EventPopupView) CreateEventFromInputs() *calendar.Event {
+    for _, v := range epv.Form.GetInputs() {
+        if !v.IsValid() {
+            return nil
+        }
+    }
+
 	name := epv.Form.GetFieldText("Name")
 	time, _ := time.Parse(TimeFormat, epv.Form.GetFieldText("Time"))
 	location := epv.Form.GetFieldText("Location")
@@ -141,8 +140,11 @@ func (epv *EventPopupView) AddEvent(g *gocui.Gui, v *gocui.View) error {
 		return nil
 	}
 
-	event := epv.CreateEventFromInputs()
-	events := event.GetReccuringEvents()
+    var newEvent *calendar.Event
+	if newEvent = epv.CreateEventFromInputs(); newEvent == nil {
+        return nil
+    }
+	events := newEvent.GetReccuringEvents()
 
 	for _, v := range events {
 		if _, err := epv.Database.AddEvent(v); err != nil {
@@ -158,7 +160,10 @@ func (epv *EventPopupView) EditEvent(g *gocui.Gui, v *gocui.View, event *calenda
 		return nil
 	}
 
-	newEvent := epv.CreateEventFromInputs()
+    var newEvent *calendar.Event
+	if newEvent = epv.CreateEventFromInputs(); newEvent == nil {
+        return nil
+    }
 	newEvent.Id = event.Id
 
 	if err := epv.Database.UpdateEventById(event.Id, newEvent); err != nil {
@@ -173,12 +178,8 @@ func (epv *EventPopupView) Close(g *gocui.Gui, v *gocui.View) error {
 	return epv.Form.Close(g, v)
 }
 
-func (epv *EventPopupView) setPopupKeybind(g *gocui.Gui, key interface{}, mod gocui.Modifier, handler func(g *gocui.Gui, v *gocui.View) error) error {
+func (epv *EventPopupView) addKeybind(key interface{}, handler func(g *gocui.Gui, v *gocui.View) error) {
 	for _, item := range epv.Form.GetItems() {
-		if err := g.SetKeybinding(item.GetLabel(), key, mod, handler); err != nil {
-			return err
-		}
+        item.AddHandlerOnly(key, handler)
 	}
-
-	return nil
 }
