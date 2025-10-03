126d711c6e96e77d653bb97301d3eb07d59a30ec
Mathieu Faucher <faucher.mathieu@gmail.com>
2025-10-02 20:58:26 -0400

    fix(notepad): Reset cursor position on clear

--- pkg/views/notepad-view.go
+++ pkg/views/notepad-view.go
@@ -12,7 +12,7 @@ type NotepadView struct {
 	*BaseView
 
 	Database *database.Database
-    IsActive bool
+	IsActive bool
 	content  string
 }
 
@@ -20,7 +20,7 @@ func NewNotepadView(c *calendar.Calendar, db *database.Database) *NotepadView {
 	tv := &NotepadView{
 		BaseView: NewBaseView("notepad"),
 		Database: db,
-        IsActive: false,
+		IsActive: false,
 	}
 
 	content, err := db.GetLatestNote()
@@ -51,7 +51,7 @@ func (npv *NotepadView) Update(g *gocui.Gui) error {
 		fmt.Fprint(v, npv.content)
 	}
 
-    npv.SaveContent(g)
+	npv.SaveContent(g)
 
 	return nil
 }
@@ -71,6 +71,7 @@ func (npv *NotepadView) ClearContent(g *gocui.Gui) error {
 		return err
 	}
 
-    v.Clear()
+	v.Clear()
+	g.CurrentView().SetCursor(0, 0)
 	return npv.SaveContent(g)
 }
